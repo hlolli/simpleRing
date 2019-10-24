e2ab797c42d6f6a5ac19ada73040a5bf99c16fc0
Hlöðver Sigurðsson <hlolli@gmail.com>
2019-10-24 12:10:23 +0200

    add github-secret payload signature digest

--- default.nix
+++ default.nix
@@ -1,6 +1,6 @@
 { stdenv, clojure, makeWrapper, fetchMavenArtifact }:
 
-let cljdeps = import ./deps.nix;
+let cljdeps = import ./deps.nix {};
     classp  = cljdeps.makeClasspaths {};
     version = "1.0.0";
 
@@ -13,11 +13,11 @@ in stdenv.mkDerivation rec {
   buildInputs = [ makeWrapper ];
 
   phases = ["installPhase"];
-  
+
   installPhase = ''
 
       mkdir -p $out/bin
-      
+
       cp ${src} $out/bin
       makeWrapper ${clojure}/bin/clojure $out/bin/simpleRing \
         --add-flags "-Scp ${classp} -i ${src} -m simple-ring" \
--- simple_ring.clj
+++ simple_ring.clj
@@ -1,21 +1,32 @@
 (ns simple-ring
   (:require [ring.adapter.jetty :refer [run-jetty]]
+            [ring.middleware.json :refer [wrap-json-body]]
+            [pandect.algo.sha1 :as sha1]
+            [crypto.equality]
             [clojure.java.shell :refer [sh]]))
 
-(defn token-auth [endpoint headers]
-  (cond (contains? endpoint :gitlab-token)
+(defn wrap-payload [handler]
+  (fn [request respond raise]
+    (let [updated-request (assoc request :payload (slurp (:body request)))]
+      (handler updated-request respond raise))))
+
+(defn token-auth [endpoint headers payload]
+  (cond (and (contains? endpoint :gitlab-token)
+             (get headers "x-gitlab-token"))
         (let [gitlab-token (get endpoint :gitlab-token)]
           (= gitlab-token (get headers "x-gitlab-token")))
-        (contains? endpoint :github-signature)
-        (let [github-signature (get endpoint :github-signature)]
-          ;; signature digest missing!
-          (= github-signature (get headers "x-hub-signature")))
-        :else true))
+        (and (contains? endpoint :github-secret)
+             (get headers "x-hub-signature"))
+        (let [github-secret     (get endpoint :github-secret)
+              ;; from https://github.com/dryewo/clj-github-app
+              payload-signature (str "sha1=" (sha1/sha1-hmac payload github-secret))]
+          (crypto.equality/eq? payload-signature (get headers "x-hub-signature")))
+        :else false))
 
 (defn create-handler [endpoints]
   (fn [request respond raise]
     (if-let [endpoint (get endpoints (:uri request))]
-      (if-not (token-auth endpoint (:headers request))
+      (if-not (token-auth endpoint (:headers request) (:payload request))
         (respond {:status 500 :body "Authorization failed!"})
         (do (respond {:status 200 :body (or (:response-body endpoint) "")})
             (when-let [script (:script endpoint)]
@@ -33,8 +44,9 @@
 (defn -main [config-path]
   (assert (not (empty? config-path)) "You must provide path to edn config file!")
   (let [{:keys [port host endpoints]} (read-string (slurp config-path))
-        handler                       (create-handler endpoints)]
+        handler                       (-> (create-handler endpoints)
+                                          (wrap-json-body {:keywords? true})
+                                          (wrap-payload))]
     (run-jetty handler {:port   (or port 1988)
                         :async? true
                         :host   (or host "127.0.0.1")})))
-
